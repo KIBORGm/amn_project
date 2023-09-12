3cd19a8e780b2256db6e6ee3b9d75baa24d652de
KIBORGm <marktar@mail.ru>
2023-09-12 12:08:16 +0200

    Добавлен класс User, содержаший данные пользователя. Добавлена ф-ция load data, отправляющая данные на сервер.

---
+++ connect.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+
+class User:
+    def __init__(self, login, pwd, username, hobby):
+        self.username = username
+        self.login = login
+        self.pwd = pwd
+        self.hobby = hobby
+
+
+def load_data(username, login, pwd, hobby): #Загружает данные пользователя в users
+    db_url = "https://amn-project-b3b8c-default-rtdb.europe-west1.firebasedatabase.app/users/.json"
+    json_data = {login: {"pwd": pwd, "username": username, "hobby": hobby}}
+
+    requests.patch(url=db_url, json=json_data)
+
