b649571757d3a5530f5ade6981976db61e79bb05
alexcodeninja <aalexdeada@gmail.com>
2023-09-12 15:24:45 +0200

    Main added

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+
+import enter
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class win_enter(enter.Ui_Form, QtWidgets.QWidget):
+	def __init__(self):
+		super().__init__()
+		self.setupUi(self)
+		self.pb_reg.clicked.connect()
+
+	def show_reg_win(self):
+		#temp
+		msg = QtWidgets.QMessageBox()
+		msg.setText("у никиты разъебано очко")
+		msg.exec_()
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    ui = win_enter()
+    ui.show()
+    sys.exit(app.exec_())
