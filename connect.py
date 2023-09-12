0929da9352084aea6a5c09e0361f06be14c0e0b9
KIBORGm <marktar@mail.ru>
2023-09-12 12:43:30 +0200

    Добавлена ф-ция delete_data, удаляющая данные пользователя

--- connect.py
+++ connect.py
@@ -10,15 +10,20 @@ class User:
         self.hobby = hobby
 
 
-def load_data(username, login, pwd, hobby): #Загружает данные пользователя в users
+def load_data(username, login, pwd, hobby):  # Загружает данные пользователя в users
     db_url = "https://amn-project-b3b8c-default-rtdb.europe-west1.firebasedatabase.app/users/.json"
     json_data = {login: {"pwd": pwd, "username": username, "hobby": hobby}}
     requests.patch(url=db_url, json=json_data)
 
-def get_data(login): #Получает данные пользователя из users
+
+def get_data(login):  # Получает данные пользователя из users по логину
     db_url = "https://amn-project-b3b8c-default-rtdb.europe-west1.firebasedatabase.app/users/"
-    res = requests.get(f"{db_url}/{login}.json").json()
+    res = requests.get(f"{db_url}/{login}.json").json() #Получение json-файла
     hobby = res['hobby']
     username = res['username']
     pwd = res['pwd']
 
+
+def delete_data(login):  # Удаляет данные из users по логину
+    db_url = "https://amn-project-b3b8c-default-rtdb.europe-west1.firebasedatabase.app/users/"
+    res = requests.delete(f"{db_url}/{login}.json").json()
