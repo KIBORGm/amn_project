659e4b55a04b69bdbd087e41fc762ec956158203
KIBORGm <marktar@mail.ru>
2023-09-12 12:33:09 +0200

    Добавлена ф-ция get_data, получающая данные из БД

--- connect.py
+++ connect.py
@@ -13,6 +13,12 @@ class User:
 def load_data(username, login, pwd, hobby): #Загружает данные пользователя в users
     db_url = "https://amn-project-b3b8c-default-rtdb.europe-west1.firebasedatabase.app/users/.json"
     json_data = {login: {"pwd": pwd, "username": username, "hobby": hobby}}
-
     requests.patch(url=db_url, json=json_data)
+
+def get_data(login): #Получает данные пользователя из users
+    db_url = "https://amn-project-b3b8c-default-rtdb.europe-west1.firebasedatabase.app/users/"
+    res = requests.get(f"{db_url}/{login}.json").json()
+    hobby = res['hobby']
+    username = res['username']
+    pwd = res['pwd']
 
